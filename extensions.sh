ec01748f5ce534fcee40146ca4cc38635e3096fa
BosEriko <bos.eriko@gmail.com>
2024-06-24 22:49:19 +0800

    update icon to city lights

--- extensions.sh
+++ extensions.sh
@@ -1,5 +1,5 @@
 code --install-extension vscodevim.vim
 code --install-extension drcika.apc-extension
 code --install-extension pmndrs.pmndrs
-code --install-extension PKief.material-product-icons
+code --install-extension Yummygum.city-lights-icon-vsc
 code --install-extension samueltscott.maximizeterminal
