be6c047cdd313b0dcc4b01dc023cc16f16887024
BosEriko <bos.eriko@gmail.com>
2024-06-24 15:02:23 +0800

    add meterial icon

--- extensions.sh
+++ extensions.sh
@@ -1,3 +1,4 @@
 codium --install-extension vscodevim.vim
 codium --install-extension drcika.apc-extension
 codium --install-extension PROxZIMA.sweetdracula
+codium --install-extension PKief.material-icon-theme
