6139f259711ace03da000d4d21ca4e7767bea6e4
BosEriko <bos.eriko@gmail.com>
2024-06-24 19:49:24 +0800

    add always max terminal settings

--- extensions.sh
+++ extensions.sh
@@ -2,3 +2,4 @@ code --install-extension vscodevim.vim
 code --install-extension drcika.apc-extension
 code --install-extension PROxZIMA.sweetdracula
 code --install-extension PKief.material-icon-theme
+code --install-extension samueltscott.maximizeterminal
