3d70654cb4bcf189b73805288c7022584a9eb915
BosEriko <bos.eriko@gmail.com>
2024-06-25 10:57:34 +0800

    add defalt control hotkeys

--- extensions.sh
+++ extensions.sh
@@ -1,5 +1,11 @@
+# Vim
 code --install-extension vscodevim.vim
+code --install-extension VSpaceCode.whichkey
+
+# Themes
 code --install-extension drcika.apc-extension
 code --install-extension pmndrs.pmndrs
 code --install-extension Yummygum.city-lights-icon-vsc
+
+# Quality of Life
 code --install-extension samueltscott.maximizeterminal
