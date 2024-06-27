0f03b603dd5f4b3f455c0fe755c4ecac88585d09
BosEriko <bos.eriko@gmail.com>
2024-06-27 09:45:01 +0800

    update extensions sorting

--- extensions.sh
+++ extensions.sh
@@ -1,15 +1,9 @@
-# Vim
-code --install-extension vscodevim.vim
-code --install-extension VSpaceCode.whichkey
-
-# Themes
-code --install-extension drcika.apc-extension
-code --install-extension pmndrs.pmndrs
-code --install-extension Yummygum.city-lights-icon-vsc
-
-# Quality of Life
-code --install-extension eamodio.gitlens
-code --install-extension samueltscott.maximizeterminal
+code --install-extension christianalexander.flip
+code --install-extension dbaeumer.vscode-eslint
 code --install-extension deibit.devdocs
-code --install-extension ethristianAlexander.flip
-code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension eamodio.gitlens
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension rebornix.ruby
+code --install-extension samueltscott.maximizeterminal
+code --install-extension wingrunr21.vscode-ruby
+code --install-extension yummygum.city-lights-icon-vsc
