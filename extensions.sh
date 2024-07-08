42478b5f2c311c5cbd63e6688d70ab15f25fc83a
BosEriko <bos.eriko@gmail.com>
2024-07-08 10:45:37 +0800

    use multi command to maximize terminal panel

--- extensions.sh
+++ extensions.sh
@@ -4,3 +4,4 @@ code --install-extension eamodio.gitlens
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension yummygum.city-lights-icon-vsc
 code --install-extension s-nlf-fh.glassit
+code --install-extension ryuta46.multi-command
